3fb32e83c0df8caf8df9ff39026f61583bd7ec5b
poojaprabhuswamy <pooja.prabhuswamy@gmail.com>
2016-04-13 22:30:48 -0700

    Added files via upload

---
+++ library/src/main/java/com/android/cmpe220/farm2home/util/CartHelper.java
@@ -0,0 +1,25 @@
+package com.android.cmpe220.farm2home.util;
+
+import com.android.cmpe220.farm2home.model.Cart;
+
+/**
+ * A helper class to retrieve the static shopping cart. Call {@code getCart()} to retrieve the shopping cart before you perform any operation on the shopping cart.
+ *
+ * @author Tony
+ */
+public class CartHelper {
+    private static Cart cart = new Cart();
+
+    /**
+     * Retrieve the shopping cart. Call this before perform any manipulation on the shopping cart.
+     *
+     * @return the shopping cart
+     */
+    public static Cart getCart() {
+        if (cart == null) {
+            cart = new Cart();
+        }
+
+        return cart;
+    }
+}
