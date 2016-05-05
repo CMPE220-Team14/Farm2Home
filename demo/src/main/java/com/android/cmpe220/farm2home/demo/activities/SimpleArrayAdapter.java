4683b62ecb1662eea64a85d1baaf6c29613139e7
poojaprabhuswamy <pooja.prabhuswamy@gmail.com>
2016-05-05 09:53:40 -0700

    Update SimpleArrayAdapter.java

--- demo/src/main/java/com/android/cmpe220/farm2home/demo/activities/SimpleArrayAdapter.java
+++ demo/src/main/java/com/android/cmpe220/farm2home/demo/activities/SimpleArrayAdapter.java
@@ -1,42 +1,145 @@
 package com.android.cmpe220.farm2home.demo.activities;
 
-import android.content.Context;
-import android.widget.ArrayAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.HashMap;
+import com.android.cmpe220.farm2home.demo.R;
+import com.android.cmpe220.farm2home.demo.adapter.CartItemAdapter;
+import com.android.cmpe220.farm2home.demo.model.CartItem;
+import com.android.cmpe220.farm2home.demo.model.Product;
+import com.android.cmpe220.farm2home.model.Cart;
+import com.android.cmpe220.farm2home.model.Saleable;
+import com.android.cmpe220.farm2home.util.CartHelper;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-public class SimpleArrayAdapter extends ArrayAdapter<String> {
+public class ShoppingCartActivity extends AppCompatActivity {
+    private static final String TAG = "ShoppingCartActivity";
 
-    Context context;
-    int textViewResourceId;
-    private static final String TAG = "SimpleArrayAdapter" ;
-    HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+    ListView lvCartItems;
+    Button bClear;
+    Button bShop;
+    Button bPay;
 
-    public SimpleArrayAdapter(Context context, int textViewResourceId,
-                              List<String> objects) {
-        super(context, textViewResourceId, objects);
-        this.context = context;
-        this.textViewResourceId = textViewResourceId;
-        for (int i = 0; i < objects.size(); ++i) {
-            hashMap.put(objects.get(i), i);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        lvCartItems = (ListView) findViewById(R.id.lvCartItems);
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        final Cart cart = CartHelper.getCart();
+        final TextView tvTotalPrice = (TextView) findViewById(R.id.tvTotalPrice);
+        tvTotalPrice.setText("$"+String.valueOf(cart.getTotalPrice()));
+
+        lvCartItems.addHeaderView(layoutInflater.inflate(R.layout.cart_header, lvCartItems, false));
+
+        final CartItemAdapter cartItemAdapter = new CartItemAdapter(this);
+
+        cartItemAdapter.updateCartItems(getCartItems(cart));
+
+        lvCartItems.setAdapter(cartItemAdapter);
+
+        bClear = (Button) findViewById(R.id.bClear);
+        bShop = (Button) findViewById(R.id.bShop);
+        bPay = (Button) findViewById(R.id.bPay);
+
+        bClear.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clearing the shopping cart");
+                cart.clear();
+                cartItemAdapter.updateCartItems(getCartItems(cart));
+                cartItemAdapter.notifyDataSetChanged();
+                tvTotalPrice.setText(String.valueOf(cart.getTotalPrice()));
+            }
+        });
+
+        bShop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShoppingCartActivity.this, ArrayAdapterListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bPay.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShoppingCartActivity.this, BillingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private List<CartItem> getCartItems(Cart cart) {
+        List<CartItem> cartItems = new ArrayList<CartItem>();
+        Log.d(TAG, "Current shopping cart: " + cart);
+
+        Map<Saleable, Integer> itemMap = (Map<Saleable, Integer>) cart.getItemWithQuantity();
+
+        for (Entry<Saleable, Integer> entry : itemMap.entrySet()) {
+            CartItem cartItem = new CartItem();
+            cartItem.setProduct((Product) entry.getKey());
+            cartItem.setQuantity(entry.getValue());
+            cartItems.add(cartItem);
         }
+
+        Log.d(TAG, "Cart item list: " + cartItems);
+        return cartItems;
     }
 
     @Override
-    public long getItemId(int position) {
-        String item = getItem(position);
-        return hashMap.get(item);
-    }
-
-    @Override
-    public boolean hasStableIds() {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
-    public void add(String object){
-        hashMap.put(object,hashMap.size());
-        this.notifyDataSetChanged();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miCart:
+                showCart();
+                return true;
+            case R.id.miProfile:
+                /*Toast.makeText(getApplicationContext(),
+                        "Profile",
+                        Toast.LENGTH_SHORT).show();*/
+                case R.id.miSignout:
+                    signout();
+                    return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void showCart()
+    {
+        Intent cartActivity = new Intent(this,ShoppingCartActivity.class);
+        startActivity(cartActivity);
+    }
+
+    public void signout()
+    {
+        Intent signoutActivity = new Intent(this, HomeActivity.class);
+        startActivity(signoutActivity);
     }
 }
